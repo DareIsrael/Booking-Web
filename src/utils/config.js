dc74f17d2b31be1170adb63fb0f525a1798d950a
DareIsrael <dareisrael4@gmail.com>
2024-03-17 20:31:29 -0700

    new commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
  
  
-//  export const BASE_URL = "https://booking-backend-2909.onrender.com/api/v1"
- export const BASE_URL = "http://localhost:4000/api/v1"
+ export const BASE_URL = "https://booking-backend-2909.onrender.com/api/v1"
+//  export const BASE_URL = "http://localhost:4000/api/v1"
